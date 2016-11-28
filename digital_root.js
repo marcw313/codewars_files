755ad58288df879db00793f1151d9093d3cfb6e7
Marc <marc.wells5@gmail.com>
2016-11-28 14:14:40 -0700

    Create digital_root.js

---
+++ digital_root.js
@@ -0,0 +1,22 @@
+
+var num = 493193;
+
+
+function digital_root(n) {
+  var total = 0;
+  var numArray = n.toString().split("")
+  if (numArray.length === 1){
+    var newNum = n;
+    return newNum;
+  }
+  else{
+    for (i = 0; i < numArray.length; i++){
+      var val = Number(numArray[i]);
+      total += val;
+    }
+  return digital_root(total)
+  }
+}
+
+
+digital_root(num);
